90b9e3563d0ab06596b8feaf39f68ecc08920ca0
Tay Zar <tayzarmyatnoe.tzmn@gmail.com>
2020-01-23 17:46:16 +0630

    - download image

--- app/src/main/java/com/ironsight/codelab/activities/ImageDownloadActivity.kt
+++ app/src/main/java/com/ironsight/codelab/activities/ImageDownloadActivity.kt
@@ -1,19 +1,26 @@
 package com.ironsight.codelab.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.ironsight.codelab.DownloadAndSaveImageTask
-import com.ironsight.codelab.R
 import kotlinx.android.synthetic.main.activity_image_download.*
+
 
 class ImageDownloadActivity : AppCompatActivity() {
 
+    @SuppressLint("SdCardPath")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_image_download)
+        setContentView(com.ironsight.codelab.R.layout.activity_image_download)
 
         download.setOnClickListener {
-            DownloadAndSaveImageTask(this).execute("https://images.wallpaperscraft.com/image/bushes_flowers_yellow_157557_300x168.jpg")
+            DownloadAndSaveImageTask(this).execute("https://images.wallpaperscraft.com/image/bridge_city_night_157572_300x168.jpg")
         }
+
+        Glide.with(this)
+            .load("/data/user/0/com.ironsight.codelab/files/Images/img.jpg")
+            .into(imageView)
     }
 }

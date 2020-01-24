3e734a4edc1155444459a1e2d2169aa4bf741fd9
Tay Zar <tayzarmyatnoe.tzmn@gmail.com>
2020-01-24 18:25:58 +0630

    - download image with permission

--- app/src/main/java/com/ironsight/codelab/DownloadAndSaveImageTask.kt
+++ app/src/main/java/com/ironsight/codelab/DownloadAndSaveImageTask.kt
@@ -3,6 +3,7 @@ package com.ironsight.codelab
 import android.content.Context
 import android.graphics.Bitmap
 import android.os.AsyncTask
+import android.os.Environment.getExternalStorageDirectory
 import android.util.Log
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -11,6 +12,8 @@ import com.bumptech.glide.request.RequestOptions
 import java.io.File
 import java.io.FileOutputStream
 import java.lang.ref.WeakReference
+import java.util.*
+
 
 class DownloadAndSaveImageTask(context: Context) : AsyncTask<String, Unit, Unit>() {
     private var mContext: WeakReference<Context> = WeakReference(context)
@@ -18,7 +21,7 @@ class DownloadAndSaveImageTask(context: Context) : AsyncTask<String, Unit, Unit>
     override fun doInBackground(vararg params: String?) {
         val url = params[0]
         val requestOptions = RequestOptions().override(100)
-            .downsample(DownsampleStrategy.CENTER_INSIDE)
+            .downsample(DownsampleStrategy.DEFAULT)
             .skipMemoryCache(true)
             .diskCacheStrategy(DiskCacheStrategy.NONE)
 
@@ -30,24 +33,47 @@ class DownloadAndSaveImageTask(context: Context) : AsyncTask<String, Unit, Unit>
                 .submit()
                 .get()
 
-            try {
-                var file = it.getDir("Images", Context.MODE_PRIVATE)
-                file = File(file, "img.jpg")
+            //try {
+//                var file = it.getDir("Images", Context.MODE_PRIVATE)
+//                file = File(file, "iamg.jpg")
+//                val out = FileOutputStream(file)
 
 //                var file = File(it.filesDir, "Images")
 //                if (!file.exists()) {
 //                    file.mkdir()
 //                }
 //                file = File(file, "img.jpg")
+//
+//                val stream = FileOutputStream(file)
+//
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 85, stream)
+//                stream.flush()
+//                stream.close()
+//                Log.i("Seiggailion", "Image saved : $stream")
+//
+//            } catch (e: Exception) {
+//                Log.i("Seiggailion", "Failed to save image.")
+//            }
 
+            val root = getExternalStorageDirectory().toString()
+            val myDir = File("$root/saved_images")
+            myDir.mkdirs()
+            val generator = Random()
+            var n = 10000
+            n = generator.nextInt(n)
+            val fName = "Image-$n.jpg"
+            val file = File(myDir, fName)
+            if (file.exists()) file.delete()
+            try {
                 val out = FileOutputStream(file)
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 85, out)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
                 out.flush()
                 out.close()
-                Log.i("Seiggailion", "Image saved to $file")
+                Log.i("Seiggailion", "Image saved : $out")
             } catch (e: Exception) {
-                Log.i("Seiggailion", "Failed to save image.")
+                e.printStackTrace()
             }
+
         }
     }
 }
--- app/src/main/java/com/ironsight/codelab/activities/ImageDownloadActivity.kt
+++ app/src/main/java/com/ironsight/codelab/activities/ImageDownloadActivity.kt
@@ -1,26 +1,98 @@
 package com.ironsight.codelab.activities
 
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
-import com.ironsight.codelab.DownloadAndSaveImageTask
+import com.ironsight.codelab.R
 import kotlinx.android.synthetic.main.activity_image_download.*
-
+import java.io.File
+import java.io.FileOutputStream
 
 class ImageDownloadActivity : AppCompatActivity() {
 
     @SuppressLint("SdCardPath")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.ironsight.codelab.R.layout.activity_image_download)
+        setContentView(R.layout.activity_image_download)
 
         download.setOnClickListener {
-            DownloadAndSaveImageTask(this).execute("https://images.wallpaperscraft.com/image/bridge_city_night_157572_300x168.jpg")
+            //DownloadAndSaveImageTask(this).execute("https://images.wallpaperscraft.com/image/man_hood_dark_157650_1280x720.jpg")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        100
+                    )
+                } else {
+                    saveImageToStorage()
+                }
+            } else {
+                saveImageToStorage()
+            }
         }
 
         Glide.with(this)
-            .load("/data/user/0/com.ironsight.codelab/files/Images/img.jpg")
+            .load("/storage/emulated/0/image.jpg")
             .into(imageView)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                saveImageToStorage()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permission is not grant, image can't save to storage.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun saveImageToStorage() {
+        val externalStorageState = Environment.getExternalStorageState()
+        if (externalStorageState == Environment.MEDIA_MOUNTED) {
+            val storageDirectory = Environment.getExternalStorageDirectory().toString()
+            val file = File(storageDirectory, "image.jpg")
+            try {
+                val stream = FileOutputStream(file)
+                val drawable = ContextCompat.getDrawable(this, R.drawable.dog_muzzle_shepherd_eyes)
+                val bitMap = (drawable as BitmapDrawable).bitmap
+                bitMap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+                stream.flush()
+                stream.close()
+                Toast.makeText(
+                    this,
+                    "Image save successfully. ${Uri.parse(file.absolutePath)}",
+                    Toast.LENGTH_LONG
+                ).show()
+                Log.d("naingwinhtun", Uri.parse(file.absolutePath).toString())
+            } catch (e: Exception) {
+                Log.d("naingwinhtun", e.toString())
+            }
+        }
     }
 }
